c99ce7514c57cdffad2561cb9407ae90c5848a53
Chris Lipinski <gooinski@gmail.com>
2022-12-10 14:05:49 +1100

    treehouse python matching game tutorial

---
+++ 2022/2023/treehouse tuts/matching game basic/card.py
@@ -0,0 +1,23 @@
+# class Card:
+# hold a card
+# matched or not?
+# location on the game board
+# __eq__
+# __str__ what to display to player
+
+class Card:
+    def __init__(self, word, location) -> None:
+        self.card = word
+        self.location = location
+        self.matched = False
+
+    def __eq__(self, other) -> bool:
+        return self.card == other.card
+
+    def __str__(self) -> str:
+        return self.card
+
+if __name__ == '__main__':
+    card1 = Card('egg', 'A1')
+    card2 = Card('egg', 'B1')
+    card3 = Card('dog', 'C1')
---
+++ 2022/2023/treehouse tuts/matching game basic/game.py
@@ -0,0 +1,15 @@
+# class Game
+# grid size
+# card options
+# columns
+# locations
+# methods
+    # create cards
+    # create grid
+    # check for matches
+    # check for game won
+    # run the game
+
+# dunder main
+    # create game instance
+    #call start game
